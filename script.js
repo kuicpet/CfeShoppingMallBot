ca41696b7c85855751c551eff9f781201760afe9
Kingsley <kingsleyumujeyan@gmail.com>
2020-12-21 00:30:17 +0100

    ft--updated scripts for totals

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const submitBtn = document.querySelector(".enter");
 const errormessage = document.querySelector(".error");
 const allowedCount = document.querySelector(".allowedCount");
 const notAllowedCount = document.querySelector(".not-allowedCount");
+const total = document.querySelector(".total-visitors");
 const faceMask = document.querySelector(".facemask");
 const clock = document.querySelector(".clock");
 const copyRight = document.querySelector(".copyright");
@@ -29,7 +30,14 @@ setInterval(() => {
 
 let allowedVisitors = [];
 let disallowedVisitors = [];
+let totalVisitors = [];
 
+// Get Total Visitors
+function getTotals() {
+    totalVisitors.push(disallowedVisitors || allowedVisitors);
+    total.innerHTML = totalVisitors.length;
+    //console.log(totalVisitors.length);
+}
 
 
 const age = 12;
@@ -46,16 +54,19 @@ const checkVisitor = () => {
         if(ageInput.value < age && faceMask.value === "yes"){
             disallowedVisitors.push(ageInput.value);
             notAllowedCount.innerHTML = disallowedVisitors.length;
+            getTotals();
             return alert("You must be 12 years or older to enter  Best Shopping Mall")
         }
         if(ageInput.value >= age && faceMask.value === "yes"){
             allowedVisitors.push(ageInput.value);
             allowedCount.innerHTML = allowedVisitors.length;
+            getTotals();
             //console.log(allowedVisitors)
             return alert("Welcome to Best Shopping Mall")
         } else {
             disallowedVisitors.push(ageInput.value);
             notAllowedCount.innerHTML = disallowedVisitors.length;
+            getTotals();
             //console.log(disallowedVisitors);
             return alert("You have been denied entry into best shopping Mall,")
         }
@@ -68,6 +79,8 @@ submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     checkVisitor();
 })
+
+
 
 
 // Footer
